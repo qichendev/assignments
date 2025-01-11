087331e2330bd66f2f120dfb72c1b1a5ee83cbf7
a_litle_tiger <38577886+XBMUAttila@users.noreply.github.com>
2025-01-10 20:24:09 -0500

    Create home-assignment-week1.py

---
+++ python-programming/home-assignment-week1.py
@@ -0,0 +1,25 @@
+questions_and_answers = [
+  "Q1": "b",
+  "Q2": "a",
+  "Q3": "d",
+  "Q4": "b",
+  "Q5": "c",
+  "Q6": "b",
+  "Q7": "c",
+  "Q8": "a",
+  "Q9": "b",
+  "Q10": "d",
+  "Q11": "b",
+  "Q12": "b",
+  "Q13": "c",
+  "Q14": "a",
+  "Q15": "d",
+  "Q16": "a",
+  "Q17": "b",
+  "Q18": "true",
+  "Q19": "true",
+  "Q20": "false"
+]
+
+for pair in questions_and_answers:
+  print pair. + "\n"
