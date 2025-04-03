34eac13433d1d2b94d75b8d117e82a24c9322e89
a_litle_tiger <frankfromcn@gmail.com>
2025-04-03 11:38:52 -0400

    .

---
+++ relational-database-design/c7/P0704.sql
@@ -0,0 +1,920 @@
+DROP DATABASE IF EXISTS RoyalProductDatabase;
+
+CREATE DATABASE RoyalProductDatabase;
+
+USE RoyalProductDatabase;
+
+CREATE TABLE RP_CUSTOMERS (
+    CUSTOMER_ID INTEGER NOT NULL,
+    CUSTOMER_NAME VARCHAR(100) NOT NULL,
+    STREET VARCHAR(100) NOT NULL,
+    CUST_CITY VARCHAR(50) NOT NULL,
+    CUST_STATE VARCHAR(50) NOT NULL,
+    ZIP VARCHAR(10) NOT NULL,
+    BALANCE DECIMAL(10, 2) NOT NULL,
+    CREDIT_LIMIT DECIMAL(10, 2) DEFAULT 5000.00 NOT NULL,
+    REP_ID INTEGER NOT NULL
+);
+
+CREATE TABLE RP_PRODUCTS (
+    PRODUCT_CODE VARCHAR(100) NOT NULL,
+    PRODUCT_DESCRIPTION VARCHAR(100) NOT NULL,
+    PRODUCT_CATEGORY VARCHAR(50) NOT NULL,
+    COST_PRICE DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE RP_INVENTORY (
+    WAREHOUSE_ID INTEGER NOT NULL,
+    PRODUCT_CODE VARCHAR(50) NOT NULL,
+    QOH INTEGER NOT NULL
+);
+
+CREATE TABLE RP_ORDERS (
+    ORDER_ID INTEGER NOT NULL,
+    ORDER_DATE DATE NOT NULL,
+    CUSTOMER_ID INTEGER NOT NULL
+);
+
+CREATE TABLE RP_ORDER_ITEMS (
+    ORDER_ID INTEGER NOT NULL,
+    PRODUCT_CODE VARCHAR(50) NOT NULL,
+    QTY_ORDERED INTEGER NOT NULL,
+    PRICE_PAID DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE RP_REPS (
+    REP_ID INTEGER NOT NULL,
+    LAST_NAME VARCHAR(60) NOT NULL,
+    FIRST_NAME VARCHAR(60) NOT NULL,
+    STREET VARCHAR(100) NOT NULL,
+    REP_CITY VARCHAR(100) NOT NULL,
+    REP_STATE VARCHAR(50) NOT NULL,
+    ZIP VARCHAR(80) NOT NULL,
+    COMMISSION DECIMAL(10, 2) NOT NULL,
+    RATE DECIMAL(10, 2) DEFAULT 4.0 NOT NULL
+);
+
+CREATE TABLE RP_WAREHOUSES (
+    WAREHOUSE_ID INTEGER NOT NULL,
+    WHSE_CITY VARCHAR(100) NOT NULL
+);
+
+ALTER TABLE
+    RP_CUSTOMERS
+ADD
+    CONSTRAINT PK_CUSTOMERS PRIMARY KEY (CUSTOMER_ID);
+
+ALTER TABLE
+    RP_PRODUCTS
+ADD
+    CONSTRAINT PK_PRODUCTS PRIMARY KEY (PRODUCT_CODE);
+
+ALTER TABLE
+    RP_INVENTORY
+ADD
+    CONSTRAINT PK_INVENTORY PRIMARY KEY (WAREHOUSE_ID, PRODUCT_CODE);
+
+ALTER TABLE
+    RP_ORDERS
+ADD
+    CONSTRAINT PK_ORDERS PRIMARY KEY (ORDER_ID);
+
+ALTER TABLE
+    RP_ORDER_ITEMS
+ADD
+    CONSTRAINT PK_ORDER_ITEMS PRIMARY KEY (ORDER_ID, PRODUCT_CODE);
+
+ALTER TABLE
+    RP_REPS
+ADD
+    CONSTRAINT PK_REPS PRIMARY KEY (REP_ID);
+
+ALTER TABLE
+    RP_WAREHOUSES
+ADD
+    CONSTRAINT PK_WAREHOUSES PRIMARY KEY (WAREHOUSE_ID);
+
+ALTER TABLE
+    RP_REPS
+ADD
+    CONSTRAINT UK_REPS UNIQUE (LAST_NAME, FIRST_NAME);
+
+ALTER TABLE
+    RP_CUSTOMERS
+ADD
+    CONSTRAINT FK_CUSTOMERS_REP FOREIGN KEY (REP_ID) REFERENCES RP_REPS(REP_ID);
+
+ALTER TABLE
+    RP_INVENTORY
+ADD
+    CONSTRAINT FK_INVENTORY_PRODUCT FOREIGN KEY (PRODUCT_CODE) REFERENCES RP_PRODUCTS(PRODUCT_CODE);
+
+ALTER TABLE
+    RP_ORDERS
+ADD
+    CONSTRAINT FK_ORDERS_CUSTOMER FOREIGN KEY (CUSTOMER_ID) REFERENCES RP_CUSTOMERS(CUSTOMER_ID);
+
+ALTER TABLE
+    RP_ORDER_ITEMS
+ADD
+    CONSTRAINT FK_ORDER_ITEMS_ORDER FOREIGN KEY (ORDER_ID) REFERENCES RP_ORDERS(ORDER_ID);
+
+ALTER TABLE
+    RP_ORDER_ITEMS
+ADD
+    CONSTRAINT FK_ORDER_ITEMS_PRODUCT FOREIGN KEY (PRODUCT_CODE) REFERENCES RP_PRODUCTS(PRODUCT_CODE);
+
+ALTER TABLE
+    RP_INVENTORY
+ADD
+    CONSTRAINT FK_INVENTORY_WAREHOUSE FOREIGN KEY (WAREHOUSE_ID) REFERENCES RP_WAREHOUSES(WAREHOUSE_ID);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (100, 'AT94', 43);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (100, 'BV06', 24);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (100, 'CD52', 21);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (100, 'DL71', 11);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (100, 'DR93', 31);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (100, 'DW11', 12);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (100, 'FD21', 12);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (100, 'KL62', 34);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (100, 'KT03', 23);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (100, 'KV29', 25);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (100, 'KV29', 25);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (200, 'AT94', 43);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (200, 'BV06', 34);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (200, 'CD52', 11);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (200, 'DL71', 41);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (200, 'DR93', 21);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (300, 'DW11', 42);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (300, 'FD21', 52);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (300, 'KL62', 14);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (300, 'KT03', 5);
+
+INSERT INTO
+    RP_INVENTORY (WAREHOUSE_ID, PRODUCT_CODE, QOH)
+VALUES
+    (300, 'KV29', 35);
+
+INSERT INTO
+    RP_WAREHOUSES (WAREHOUSE_ID, WHSE_CITY)
+VALUES
+    (100, 'Chicago');
+
+INSERT INTO
+    RP_WAREHOUSES (WAREHOUSE_ID, WHSE_CITY)
+VALUES
+    (200, 'Detroit');
+
+INSERT INTO
+    RP_WAREHOUSES (WAREHOUSE_ID, WHSE_CITY)
+VALUES
+    (300, 'Toronto');
+
+INSERT INTO
+    RP_REPS (
+        REP_ID,
+        LAST_NAME,
+        FIRST_NAME,
+        STREET,
+        REP_CITY,
+        REP_STATE,
+        ZIP,
+        COMMISSION,
+        RATE
+    )
+VALUES
+    (
+        20,
+        'Culp',
+        'Betty',
+        '1275 Main St',
+        'Detroit',
+        'MI',
+        '48288',
+        20542.50,
+        0.05
+    );
+
+INSERT INTO
+    RP_REPS (
+        REP_ID,
+        LAST_NAME,
+        FIRST_NAME,
+        STREET,
+        REP_CITY,
+        REP_STATE,
+        ZIP,
+        COMMISSION,
+        RATE
+    )
+VALUES
+    (
+        35,
+        'Manis',
+        'Richard',
+        '532 Jackson',
+        'Toronto',
+        'ON',
+        'M5V22K1',
+        39216.00,
+        0.0
+    );
+
+INSERT INTO
+    RP_REPS (
+        REP_ID,
+        LAST_NAME,
+        FIRST_NAME,
+        STREET,
+        REP_CITY,
+        REP_STATE,
+        ZIP,
+        COMMISSION,
+        RATE
+    )
+VALUES
+    (
+        65,
+        'Large',
+        'Tom',
+        '1626 Taylor',
+        'Chicago',
+        'IL',
+        '60099',
+        23487.00,
+        0.05
+    );
+
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        148,
+        'Als Appliance and Sport',
+        '2837 Greenway',
+        'Detroit',
+        'MI',
+        '48244',
+        6550.00,
+        7500.00,
+        20
+    );
+
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        282,
+        'Brookings Direct',
+        '3827 Devon',
+        'Toronto',
+        'ON',
+        'M5V7F5',
+        431.50,
+        10000.00,
+        35
+    );
+
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        356,
+        'Fergusons',
+        '382 Wildwood',
+        'Northfield',
+        'MI',
+        '33146',
+        5785.00,
+        7500.00,
+        20
+    );
+
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        408,
+        'The Everything Shop',
+        '1828 Raven',
+        'Crystal',
+        'IL',
+        60082,
+        4285.25,
+        5000.00,
+        65
+    );
+
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        462,
+        'Bargains Galore',
+        '3829 Central',
+        'Toronto',
+        'ON',
+        'M5V9G4',
+        3412.00,
+        10000.00,
+        35
+    );
+
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        524,
+        'Klines',
+        '838 Ridgeland',
+        'Lakeside',
+        'IL',
+        60091,
+        12762.00,
+        15000.00,
+        65
+    );
+
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        608,
+        'Johnsons Department Store',
+        '372 Oxford',
+        'Toronto',
+        'ON',
+        'M5V9S4',
+        2106.00,
+        10000.00,
+        35
+    );
+
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        687,
+        'Lees Sport and Appliance',
+        '282 Evergreen',
+        'Troy',
+        'MI',
+        48283,
+        2851.00,
+        5000.00,
+        20
+    );
+
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        725,
+        'Deerfields Four Seasons',
+        '282 Columbia',
+        'Toronto',
+        'ON',
+        'M5V9J5',
+        248.00,
+        7500.00,
+        35
+    );
+
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        842,
+        'All Season',
+        '28 Lakeview',
+        'Grove City',
+        'IL',
+        60081,
+        6221.00,
+        7500.00,
+        65
+    );
+
+INSERT INTO
+    RP_ORDER_ITEMS (ORDER_ID, PRODUCT_CODE, QTY_ORDERED, PRICE_PAID)
+VALUES
+    (21608, 'AT94', 11, 21.95);
+
+INSERT INTO
+    RP_ORDER_ITEMS (ORDER_ID, PRODUCT_CODE, QTY_ORDERED, PRICE_PAID)
+VALUES
+    (21610, 'DR93', 1, 495.00);
+
+INSERT INTO
+    RP_ORDER_ITEMS (ORDER_ID, PRODUCT_CODE, QTY_ORDERED, PRICE_PAID)
+VALUES
+    (21610, 'DW11', 1, 399.99);
+
+INSERT INTO
+    RP_ORDER_ITEMS (ORDER_ID, PRODUCT_CODE, QTY_ORDERED, PRICE_PAID)
+VALUES
+    (21613, 'KL62', 4, 329.95);
+
+INSERT INTO
+    RP_ORDER_ITEMS (ORDER_ID, PRODUCT_CODE, QTY_ORDERED, PRICE_PAID)
+VALUES
+    (21614, 'KT03', 2, 595.00);
+
+INSERT INTO
+    RP_ORDER_ITEMS (ORDER_ID, PRODUCT_CODE, QTY_ORDERED, PRICE_PAID)
+VALUES
+    (21617, 'BV06', 2, 794.95);
+
+INSERT INTO
+    RP_ORDER_ITEMS (ORDER_ID, PRODUCT_CODE, QTY_ORDERED, PRICE_PAID)
+VALUES
+    (21617, 'CD52', 4, 150.00);
+
+INSERT INTO
+    RP_ORDER_ITEMS (ORDER_ID, PRODUCT_CODE, QTY_ORDERED, PRICE_PAID)
+VALUES
+    (21619, 'DR93', 1, 495.00);
+
+INSERT INTO
+    RP_ORDER_ITEMS (ORDER_ID, PRODUCT_CODE, QTY_ORDERED, PRICE_PAID)
+VALUES
+    (21623, 'KV29', 2, 1290.00);
+
+INSERT INTO
+    RP_ORDERS (ORDER_ID, ORDER_DATE, CUSTOMER_ID)
+VALUES
+    (21608, '2021-01-20', 148);
+
+INSERT INTO
+    RP_ORDERS (ORDER_ID, ORDER_DATE, CUSTOMER_ID)
+VALUES
+    (21610, '2021-02-20', 356);
+
+INSERT INTO
+    RP_ORDERS (ORDER_ID, ORDER_DATE, CUSTOMER_ID)
+VALUES
+    (21613, '2021-02-21', 408);
+
+INSERT INTO
+    RP_ORDERS (ORDER_ID, ORDER_DATE, CUSTOMER_ID)
+VALUES
+    (21614, '2021-03-21', 282);
+
+INSERT INTO
+    RP_ORDERS (ORDER_ID, ORDER_DATE, CUSTOMER_ID)
+VALUES
+    (21617, '2021-03-22', 608);
+
+INSERT INTO
+    RP_ORDERS (ORDER_ID, ORDER_DATE, CUSTOMER_ID)
+VALUES
+    (21619, '2021-04-23', 148);
+
+INSERT INTO
+    RP_ORDERS (ORDER_ID, ORDER_DATE, CUSTOMER_ID)
+VALUES
+    (21623, '2021-04-23', 608);
+
+INSERT INTO
+    RP_PRODUCTS (
+        PRODUCT_CODE,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_CATEGORY,
+        COST_PRICE
+    )
+VALUES
+    ('AT94', 'Iron', 'HW', 24.95);
+
+INSERT INTO
+    RP_PRODUCTS (
+        PRODUCT_CODE,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_CATEGORY,
+        COST_PRICE
+    )
+VALUES
+    ('BV06', 'Home Gym', 'SG', 794.95);
+
+INSERT INTO
+    RP_PRODUCTS (
+        PRODUCT_CODE,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_CATEGORY,
+        COST_PRICE
+    )
+VALUES
+    ('CD52', 'Microwave Oven', 'AP', 165.00);
+
+INSERT INTO
+    RP_PRODUCTS (
+        PRODUCT_CODE,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_CATEGORY,
+        COST_PRICE
+    )
+VALUES
+    ('DL71', 'Cordless Drill', 'HW', 129.95);
+
+INSERT INTO
+    RP_PRODUCTS (
+        PRODUCT_CODE,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_CATEGORY,
+        COST_PRICE
+    )
+VALUES
+    ('DR93', 'Gas Range', 'AP', 495.00);
+
+INSERT INTO
+    RP_PRODUCTS (
+        PRODUCT_CODE,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_CATEGORY,
+        COST_PRICE
+    )
+VALUES
+    ('DW11', 'Washer', 'AP', 399.99);
+
+INSERT INTO
+    RP_PRODUCTS (
+        PRODUCT_CODE,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_CATEGORY,
+        COST_PRICE
+    )
+VALUES
+    ('FD21', 'Stand Mixer', 'HW', 159.95);
+
+INSERT INTO
+    RP_PRODUCTS (
+        PRODUCT_CODE,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_CATEGORY,
+        COST_PRICE
+    )
+VALUES
+    ('KL62', 'Dryer', 'AP', 349.95);
+
+INSERT INTO
+    RP_PRODUCTS (
+        PRODUCT_CODE,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_CATEGORY,
+        COST_PRICE
+    )
+VALUES
+    ('KT03', 'Dishwasher', 'AP', 595.00);
+
+INSERT INTO
+    RP_PRODUCTS (
+        PRODUCT_CODE,
+        PRODUCT_DESCRIPTION,
+        PRODUCT_CATEGORY,
+        COST_PRICE
+    )
+VALUES
+    ('KV29', 'Treadmill', 'SG', 1390.00);
+
+--testing
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        200,
+        'Duplicate Entry',
+        '555 Test Ave',
+        'Sample City',
+        'SC',
+        '55555',
+        6200.00,
+        8200.00,
+        35
+    );
+
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        888,
+        'Invalid Rep Entry',
+        '321 Fake Blvd',
+        'No Town',
+        'NT',
+        '88888',
+        4300.00,
+        6900.00,
+        888
+    );
+
+INSERT INTO
+    RP_REPS (
+        REP_ID,
+        LAST_NAME,
+        FIRST_NAME,
+        STREET,
+        REP_CITY,
+        REP_STATE,
+        ZIP,
+        COMMISSION,
+        RATE
+    )
+VALUES
+    (
+        88,
+        'Smith',
+        'John',
+        '111 Market St',
+        'Nowhere',
+        'NW',
+        '11111',
+        12000.00,
+        6.0
+    );
+
+UPDATE
+    RP_CUSTOMERS
+SET
+    BALANCE = -750
+WHERE
+    CUSTOMER_ID = 200;
+
+UPDATE
+    RP_CUSTOMERS
+SET
+    CREDIT_LIMIT = -2500
+WHERE
+    CUSTOMER_ID = 200;
+
+DELETE FROM
+    RP_REPS
+WHERE
+    REP_ID = 35;
+
+INSERT INTO
+    RP_CUSTOMERS (
+        CUSTOMER_ID,
+        CUSTOMER_NAME,
+        STREET,
+        CUST_CITY,
+        CUST_STATE,
+        ZIP,
+        BALANCE,
+        CREDIT_LIMIT,
+        REP_ID
+    )
+VALUES
+    (
+        777,
+        'New Valid Entry',
+        '987 Valid St',
+        'Valid City',
+        'VC',
+        '77777',
+        2500.00,
+        7000.00,
+        35
+    );
+
+UPDATE
+    RP_CUSTOMERS
+SET
+    CREDIT_LIMIT = 15000
+WHERE
+    CUSTOMER_ID = 777;
+
+DELETE FROM
+    RP_CUSTOMERS
+WHERE
+    CUSTOMER_ID = 777;
+
+UPDATE
+    RP_CUSTOMERS
+SET
+    BALANCE = 4285.25
+WHERE
+    CUSTOMER_ID = 408;
+
+UPDATE
+    RP_CUSTOMERS
+SET
+    BALANCE = 6221.00
+WHERE
+    CUSTOMER_ID = 842;
+
+UPDATE
+    RP_PRODUCTS
+SET
+    COST_PRICE = COST_PRICE + 1.085
+WHERE
+    PRODUCT_CATEGORY = 'AP';
+
+DELETE FROM
+    RP_PRODUCTS
+WHERE
+    PRODUCT_DESCRIPTION = 'Cordless Drill';
+
+UPDATE
+    RP_CUSTOMERS
+SET
+    CREDIT_LIMIT = 14500
+WHERE
+    CUSTOMER_NAME = 'Brookings Direct';
