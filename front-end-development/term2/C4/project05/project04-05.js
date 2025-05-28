282bec4dce5bd91ec11839d9e172f55019b1bb2e
Qi Chen <38577886+FrankFromCN@users.noreply.github.com>
2025-05-28 20:03:01 +0000

    .

---
+++ front-end-development/term2/C4/project05/project04-05.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/*    JavaScript 7th Edition
+      Chapter 4
+      Project 04-05
+
+      Degrees <-> Radians Coverter
+      Author: Qi Chen
+      Date:   May 28, 2025
+
+      Filename: project04-05.js
+ */
+
+const PI = 3.14159265358979323846; // Define the value of PI
+
+// Function to convert degrees to radians 
+function degreesToRadians(degrees) {
+   return degrees*PI/180;
+}
+
+// Function to convert radians to degrees
+function radiansToDegrees(radians) {
+   return radians*180/PI;
+}
+
+// Event handler that converts radians to degrees when the input box is changed
+document.getElementById("rValue").onchange = function() {
+   let radians = document.getElementById("rValue").value;
+   document.getElementById("aValue").value = formatValue3(radiansToDegrees(radians));
+}
+
+// Event handler that converts degrees to radians when the input box is changed
+document.getElementById("aValue").onchange = function() {
+   let degrees = document.getElementById("aValue").value;
+   document.getElementById("rValue").value = formatValue3(degreesToRadians(degrees));
+}
+
+
+
+
+
+/* ================================================================= */
+ // Function to display a numeric value in the format ##.### 
+ function formatValue3(value) {
+    return value.toFixed(3);
+ }
