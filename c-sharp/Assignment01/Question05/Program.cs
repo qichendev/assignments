bb4891f3a22ee9c7671fc362e17c6a0466f287c5
a_litle_tiger <frankfromcn@gmail.com>
2025-06-01 18:22:29 -0400

    .

--- c-sharp/Assignment01/Question05/Program.cs
+++ c-sharp/Assignment01/Question05/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace Question05
 {
-    internal class Program
+    public class RoShamBoEngine
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            string[] choices = { "rock", "paper", "scissors" };
+            Random random = new Random();
+            int computerChoice = random.Next(choices.Length);
+            Console.WriteLine("Rock, Paper, Scissors!");
+            Console.WriteLine("Enter your choice: ");
+            string userChoice = Console.ReadLine();
+            Console.WriteLine("Computer chose: " + choices[computerChoice]);
+            if (userChoice.ToLower() == choices[computerChoice])
+            {
+                Console.WriteLine("It's a draw!");
+            }
+            else if (userChoice.ToLower() == "rock" && choices[computerChoice] == "scissors")
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (userChoice.ToLower() == "paper" && choices[computerChoice] == "rock")
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (userChoice.ToLower() == "scissors" && choices[computerChoice] == "paper")
+            {
+                Console.WriteLine("You win!");
+            }
+            else
+            {
+                Console.WriteLine("You lose!");
+            }
         }
     }
 }
--- c-sharp/Assignment01/Question06/Program.cs
+++ c-sharp/Assignment01/Question06/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace Question06
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        class WordWithVowels
         {
+            public string word;
+            public int vowels;
+        }
+        public static void Main(string[] args)
+        {
+            TextReader reader = new StreamReader("words.txt");
+            Queue<WordWithVowels> lines = new Queue<WordWithVowels>();
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                lines.Enqueue(new WordWithVowels { word = line, vowels = 0 });
+            }
+            reader.Close();
+            var vowels = new HashSet<char> {'a', 'e', 'i', 'o', 'u'};
+            int maxVowels = 0;
+            for (int i = 0; i < lines.Count; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    if (vowels.Contains(lines[i].word[j].ToLower()))
+                    {
+                        lines[i].vowels++;
+                    }
+                }
+                if (lines[i].vowels > maxVowels)
+                {
+                    maxVowels = lines[i].vowels;
+                }
+            }
+            Console.WriteLine("The largest number of vowels in any one word is: " + maxVowels);
         }
     }
 }
