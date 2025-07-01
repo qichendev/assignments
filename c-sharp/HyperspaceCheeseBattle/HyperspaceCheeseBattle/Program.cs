52f1d2da2a6480969a93ddfcf4bb4cc6ddee1713
Qi Chen <38577886+FrankFromCN@users.noreply.github.com>
2025-07-01 19:13:36 -0300

    .

---
+++ c-sharp/HyperspaceCheeseBattle/HyperspaceCheeseBattle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperspaceCheeseBattle
+{
+    internal class Program
+    {
+        enum Direction{
+            Up,
+            Down,
+            Left,
+            Right,
+            Win
+        };
+        static int[,] board = new int[,] {
+            Up,
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
